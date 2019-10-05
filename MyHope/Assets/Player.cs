9b12daafea9b237473c3d089f801e42123e9633d
fiwon123 <felipeinoue@yahoo.com.br>
2019-10-04 23:32:14 -0300

    Start project

---
+++ MyHope/Assets/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb = null;
+    Collider2D collider;
+    float distToGround = 0f;
+
+    Vector2 motion;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+        distToGround = collider.bounds.extents.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    bool IsGround()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+    }
+
+    void Move()
+    {
+        motion.x = Input.GetAxis("Horizontal");
+
+        rb.velocity = motion * 10;
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsGround())
+        {
+            motion.y = 100;
+            rb.velocity = motion * 10;
+        }
+        else
+        {
+
+        }
+    }
+}
