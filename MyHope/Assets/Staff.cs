c5b79c96bfa6bb222c0e776eaa1dff25334d65ed
fiwon123 <felipeinoue@yahoo.com.br>
2019-10-05 12:17:16 -0300

    Start fix problem in particle attack

---
+++ MyHope/Assets/Staff.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Staff : MonoBehaviour
+{
+    float hp = 100;
+
+    private void OnParticleTrigger()
+    {
+        hp -= 10;
+
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
